0ff0c555624f4955608b29958412fcbc11d6b702
Jose Sa <jose-a-sa@users.noreply.github.com>
2025-09-06 20:06:32 +0100

    integrated tests via CTest

--- build_and_run_tests.sh
+++ build_and_run_tests.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 
-cmake -B build -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release -j8
+# configure
+cmake -B build -DCMAKE_BUILD_TYPE=Release
 
-for t in tests/*.in; do
-    name=$(basename "$t" .in);
+# build
+cmake --build build --config Release -j8
 
-    in_file="tests/$name.in"
-    out_file="tests/$name.out"
+# run tests
+ctest --test-dir build -C Release -T test --output-on-failure -j8
 
-    [[ ! -f "$in_file" ]] && continue;
 
-    diff -w <(build/balance-scales < "$in_file") <(cat "$out_file")
-    [ $? -eq 0 ] && echo "$name: PASS" || echo "$name: FAIL"
-done
+# for t in tests/*.in; do
+#     name=$(basename "$t" .in);
+
+#     in_file="tests/$name.in"
+#     out_file="tests/$name.out"
+
+#     [[ ! -f "$out_file" ]] && continue;
+
+#     diff -w <(build/balance-scales < "$in_file") <(cat "$out_file")
+#     [ $? -eq 0 ] && echo "$name: PASS" || echo "$name: FAIL"
+# done
